6ece299b75fdddcaeb8d8660d2e7e20dbb049bb6
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-02-16 07:46:59 -0500

    Finished chapter 5 + Silver Challenge

---
+++ chapter5-viewController/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Nithi Prasanpanich on 2/16/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		print("MapViewController loaded its view")
+	}
+}
