38b3d48b591f850c4a10d50739a932aa968f8e3c
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-03-01 18:29:59 -0500

    Finished ch 10 + 11

---
+++ chapter6-ProgramaticView/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Nithi Prasanpanich on 2/14/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+	
+	
+	//SILVER CHALLENGE
+	override func viewWillAppear(thing: Bool) {
+		super.viewWillAppear(thing);
+		let hour = NSCalendar.currentCalendar().component(.Hour, fromDate: NSDate())
+		print(hour)
+		if hour >= 17 { // Dim background if current time pass 5:00pm
+			self.view.backgroundColor = UIColor(red: 0.1137, green: 0.1725, blue: 0.4, alpha: 1.0)
+		}else if hour >= 0 && hour <= 6 {
+			self.view.backgroundColor = UIColor(red: 0.1137, green: 0.1725, blue: 0.4, alpha: 1.0)
+		}else{ // ligten background during daytime
+			self.view.backgroundColor = UIColor(red: 0.7176, green: 0.9176, blue: 1, alpha: 1.0)
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		print("ConversionViewController loaded its view")
+	}
+	
+
+
+	@IBOutlet var celsiusLabel: UILabel!
+	@IBOutlet var textField: UITextField!
+	
+	let numberFormatter: NSNumberFormatter = {
+		let nf = NSNumberFormatter()
+		nf.numberStyle = .DecimalStyle
+		nf.minimumFractionDigits = 0
+		nf.maximumFractionDigits = 1
+		return nf
+	}()
+	
+	func updateCelsiusLabel() {
+		if let value = celsiusValue {
+			celsiusLabel.text = numberFormatter.stringFromNumber(value)
+		}
+		else{
+			celsiusLabel.text = "???"
+		}
+	}
+	
+	
+	var fahrenheitValue: Double? {
+		didSet {
+			updateCelsiusLabel()
+		}
+	}
+	
+	var celsiusValue: Double? {
+		if let value = fahrenheitValue{
+			return (value-32) * (5/9)
+		}else{
+			return nil
+		}
+	}
+	
+
+	@IBAction func dismissKeyboard(sender: AnyObject){
+		textField.resignFirstResponder()
+	}
+	
+	
+	@IBAction func fahrenheitFieldEdittingChanged(textField: UITextField) {
+		if let text = textField.text, value = Double(text) {
+			fahrenheitValue = value
+		}else{
+			fahrenheitValue = nil
+		}
+	}
+	
+	
+
+	
+	func textField(textField: UITextField,
+		shouldChangeCharactersInRange range: NSRange,
+		replacementString string:String) -> Bool {
+			
+			let existingTextHasDecimalSeperator = textField.text?.rangeOfString(".")
+			let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+
+			
+			if existingTextHasDecimalSeperator != nil && replacementTextHasDecimalSeparator != nil {
+				return false
+			}
+			
+			else {
+				return true
+			}
+			
+	}
+
+
+	
+}
---
+++ chapter6-ProgramaticView/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Nithi Prasanpanich on 2/16/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+	
+	var mapView: MKMapView!
+	override func loadView() {
+		//Create a map view
+		mapView = MKMapView()
+		
+		//Set it as the view of this view controller
+		view = mapView
+		
+		let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+		segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+		segmentedControl.selectedSegmentIndex = 0
+		
+		segmentedControl.translatesAutoresizingMaskIntoConstraints = false //Prevent confliction
+		view.addSubview(segmentedControl)
+		
+		let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+		let margins = view.layoutMarginsGuide
+		let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+		let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+		
+		topConstraint.active = true
+		leadingConstraint.active = true
+		trailingConstraint.active = true
+		
+		
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		print("MapViewController loaded its view")
+	}
+	
+}
+
