d62efbec167eb06d8d170bce8db377c6fd7b568f
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-03-19 23:15:19 -0400

    Random values

---
+++ chapter9-tableView/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Nithi Prasanpanich on 3/8/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+
+
+class ItemsViewController: UITableViewController{
+	
+}
