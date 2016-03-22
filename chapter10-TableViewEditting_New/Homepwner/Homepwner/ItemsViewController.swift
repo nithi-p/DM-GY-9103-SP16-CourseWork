1e9ed1fd058d1c7002a0ce3bbfe736130404e68b
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-03-22 16:28:57 -0400

    Chapter 10 revisit

---
+++ chapter10-TableViewEditting_New/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Nithi Prasanpanich on 3/8/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+
+
+class ItemsViewController: UITableViewController{
+	
+	
+	var itemStore: ItemStore!
+	
+	@IBAction func addNewItem(sender: AnyObject) {
+		print("add")
+		
+	}
+	
+	@IBAction func toggleEditingMode(sender: AnyObject){
+		print("edit")
+		
+	}
+	
+	override func tableView(tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+		return itemStore.allItems.count
+	}
+	
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+		let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+		let item = itemStore.allItems[indexPath.row]
+		
+		cell.textLabel?.text = item.name
+		cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+		
+		return cell
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+		
+		let insets = UIEdgeInsets(top: statusBarHeight, left:0, bottom:0, right:0)
+		tableView.contentInset = insets
+		tableView.scrollIndicatorInsets = insets
+	}
+	
+}
