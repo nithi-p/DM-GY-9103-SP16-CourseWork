e7bdf8c8611fdbba6668d29b7af47968c0048539
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-03-29 17:30:08 -0400

    Dismissing keyboard using return key

---
+++ chapter13-UINav/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,70 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Nithi Prasanpanich on 3/29/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate {
+	
+	@IBOutlet var nameField: UITextField!
+	@IBOutlet var serialNumberField: UITextField!
+	@IBOutlet var valueField: UITextField!
+	@IBOutlet var dateLabel: UILabel!
+	
+	var item: Item!
+	
+	/* formatter */
+	
+	let numberFormatter: NSNumberFormatter = {
+		let formatter = NSNumberFormatter()
+		formatter.numberStyle = .DecimalStyle
+		formatter.minimumFractionDigits = 2
+		formatter.maximumFractionDigits = 2
+		return formatter
+	}()
+	
+	let dateFormatter: NSDateFormatter = {
+		let formatter = NSDateFormatter()
+		formatter.dateStyle = .MediumStyle
+		formatter.timeStyle = .NoStyle
+		return formatter
+	}()
+	
+	
+	/* formatter ends */
+	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		nameField.text = item.name
+		serialNumberField.text = item.serialNumber
+		/*valueField.text = "\(item.valueInDollars)"
+		dateLabel.text = "\(item.dateCreated)"*/
+		valueField.text = numberFormatter.stringFromNumber(item.valueInDollars)
+		dateLabel.text  = dateFormatter.stringFromDate(item.dateCreated)
+	}
+	
+	override func viewWillDisappear(animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		//"Save" Changes to item
+		item.name = nameField.text ?? ""
+		item.serialNumber = serialNumberField.text
+		
+		if let valueText = valueField.text, value = numberFormatter.numberFromString(valueText) {
+				item.valueInDollars = value.integerValue
+			
+		}else{
+			item.valueInDollars = 0
+		}
+	}
+	
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+}
