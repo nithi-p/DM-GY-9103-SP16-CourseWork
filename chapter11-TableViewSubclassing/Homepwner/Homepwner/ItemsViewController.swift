612bd0ed3a352c98a5a07e9f501f94a576cf8ed5
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-03-29 12:20:32 -0400

    Done Bronze challenge on chapter 11

--- chapter11-TableViewSubclassing/Homepwner/Homepwner/ItemsViewController.swift
+++ chapter11-TableViewSubclassing/Homepwner/Homepwner/ItemsViewController.swift
@@ -23,6 +23,12 @@ class ItemsViewController: UITableViewController{
 	
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
 		let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
+		
+		//Update the label for the new size
+		cell.updateLabels()
+		
+		
+		
 		let item = itemStore.allItems[indexPath.row]
 		
 		//configure the cell with the item
@@ -30,6 +36,13 @@ class ItemsViewController: UITableViewController{
 		cell.serialNumberLabel.text = item.serialNumber
 		cell.valueLabel.text = "$\(item.valueInDollars)"
 		
+		
+		//Bronze challenge
+		if ( item.valueInDollars < 50 ) {
+			cell.valueLabel.textColor = UIColor.greenColor()
+		} else {
+			cell.valueLabel.textColor = UIColor.redColor()
+		}
 		
 		
 		return cell
@@ -111,7 +124,8 @@ class ItemsViewController: UITableViewController{
 		tableView.contentInset = insets
 		tableView.scrollIndicatorInsets = insets
 		
-		tableView.rowHeight = 65
+		tableView.rowHeight = UITableViewAutomaticDimension
+		tableView.estimatedRowHeight = 65
 	}
 	
 }
